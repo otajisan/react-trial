634e9c109d8fd5a1dd52ec5130265bdc430bb704
otajisan <taji.taji.otajisan@gmail.com>
2021-11-04 19:37:29 +0900

    :+1: add: web request sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {GreetingMessage} from "./components/GreetingMessage";
+import axios from "axios";
 
-function App() {
+
+const App = () => {
+  const greetingApiUrl: string = 'http://localhost:8080';
+  type GreetingResponse = {
+    message: string;
+  };
+  // state
+  const [response, setResponse] = useState<GreetingResponse>({message: "(click button and then greeting message here.)"});
+
+  const onClickButton = () => {
+    axios.get<GreetingResponse>(greetingApiUrl).then((res) => {
+      console.log(res);
+      setResponse(res.data);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Hello My First React!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 style={{color: 'skyblue'}}>Hello My First React!</h1>
+        <button onClick={onClickButton}>get greeting message</button>
+        <GreetingMessage>{response.message}</GreetingMessage>
       </header>
     </div>
   );
